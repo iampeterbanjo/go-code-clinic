2ac29814e2a4c260e8fe2ca766b01e8316a4e356
iampeterbanjo <peterbanjo@gmail.com>
2016-02-25 12:57:09 +0000

    Add lake pend exercise description

---
+++ exploring-lake-pend-orielle.go
@@ -0,0 +1,7 @@
+# Calculate the mean and median of the Wind speed, Air temperature and Barometric pressure
+# recorded at Deep Moor station for a given range of dates
+# Mean      = sum of items / number of items
+# Median    = if odd the middle item else if even sum of two middle numbers divided by two
+# 
+# Data source http://lpo.dt.navy.mil
+
